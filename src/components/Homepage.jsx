349beece6e0ccabc2db2626e74dd8032bdbf3240
Quentin Chin <q.chin@hotmail.com>
2022-01-22 15:45:33 -0500

    Add crypto stats and implement antd

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,13 +6,17 @@ import { Link } from "react-router-dom";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
+import { Cryptocurrencies } from "../components";
+import { News } from "../components";
+
 const { Title } = Typography;
 // Destructuring Title from typography don't have to type Typography.Title
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery();
+  const globalStats = data?.data?.stats;
 
-  console.log(data);
+  if (isFetching) return "Loading...";
 
   return (
     <>
@@ -24,21 +28,51 @@ const Homepage = () => {
           span={12}
           // Antd uses a span of 24 spaces, whereas MUI uses 12 spaces!
         >
-          <Statistic title="Total Cryptocurrencies" value="5" />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value="5" />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies Worldwide
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News{" "}
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
