3be0ce1495c586946e1f87fdbd4ba4d06ee515d9
Quentin Chin <q.chin@hotmail.com>
2022-01-24 20:52:18 -0500

    Comment out timestap error

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,10 +1,47 @@
 import React from "react";
-import { Line } from "react-chartjs-2";
+// import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
 
 const { Title } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  //   const coinPrice = [];
+  //   const coinTimestamp = [];
+
+  //   for (let i = 0; i < coinHistory?.data?.history?.length; i++) {
+  //     coinPrice.push(coinHistory?.data?.history[i].price);
+  //   }
+
+  //   for (let i = 0; i < coinHistory?.data?.history?.length; i++) {
+  //     coinTimestamp.push(
+  //       new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
+  //     );
+  //   }
+  //   const data = {
+  //     labels: coinTimestamp,
+  //     datasets: [
+  //       {
+  //         label: "Price In USD",
+  //         data: coinPrice,
+  //         fill: false,
+  //         backgroundColor: "#0071bd",
+  //         borderColor: "#0071bd",
+  //       },
+  //     ],
+  //   };
+
+  //   const options = {
+  //     scales: {
+  //       yAxes: [
+  //         {
+  //           ticks: {
+  //             beginAtZero: true,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   };
+
   return (
     <>
       <Row className="chart-header">
@@ -20,6 +57,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
           </Title>
         </Col>
       </Row>
+      {/* <Line data={data} options={options} /> */}
     </>
   );
 };
